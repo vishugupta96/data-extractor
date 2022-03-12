9685f06a5ae8f3c84fc24748ff997c389086642a
your_username~ <30354305+vishugupta96@users.noreply.github.com>
2022-03-12 14:53:57 +0530

    update settings

--- datamine/settings.py
+++ datamine/settings.py
@@ -119,6 +119,8 @@ LOGIN_REDIRECT_URL = '/tutorials'
 
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -152,7 +154,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -173,8 +176,6 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
